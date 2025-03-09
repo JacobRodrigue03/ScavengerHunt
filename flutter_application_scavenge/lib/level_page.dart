a3a80f370f92fad2fcfc3adec9abdaaedcdcdf9f
Steven Reed <stevenreed122@gmail.com>
2025-03-09 14:05:43 -0500

    Made images appear on level page

--- flutter_application_scavenge/lib/level_page.dart
+++ flutter_application_scavenge/lib/level_page.dart
@@ -8,36 +8,62 @@ class LevelPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the map image path based on the level
+    String _mapImage;
+    switch (level) {
+      case '1':
+        _mapImage = 'assets/images/floor_1_pft.jpg';
+        break;
+      case '2':
+        _mapImage = 'assets/images/floor_2_pft.jpg';
+        break;
+      case '3':
+        _mapImage = 'assets/images/floor_3_pft.jpg';
+        break;
+      default:
+        _mapImage = 'assets/images/floor_1_pft.jpg';
+        break;
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text('Level $level')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Explore Level $level', style: const TextStyle(fontSize: 24)),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) =>
-                          LocationPage(level: level, locationNumber: 1)),
-                );
-              },
-              child: const Text('Location 1'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (_) =>
-                          LocationPage(level: level, locationNumber: 2)),
-                );
-              },
-              child: const Text('Location 2'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(_mapImage),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Explore Level $level',
+                  style: const TextStyle(fontSize: 24)),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) =>
+                            LocationPage(level: level, locationNumber: 1)),
+                  );
+                },
+                child: const Text('Location 1'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) =>
+                            LocationPage(level: level, locationNumber: 2)),
+                  );
+                },
+                child: const Text('Location 2'),
+              ),
+            ],
+          ),
         ),
       ),
     );
